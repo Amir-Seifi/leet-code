61bc667a376a233fe864b7b2bb4f2dbe8209684c
grimnir <amir.mseifi93@gmail.com>
2025-02-26 12:18:56 +0330

    palindrome

---
+++ src/class-problems/palindrome-number/9.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+
+var isPalindrome = function (x) {
+  const strX = x.toString();
+  let result = "";
+
+  for (let i = strX.length - 1; i >= 0; i--) {
+    result += strX[i];
+  }
+
+  return strX === result;
+};
