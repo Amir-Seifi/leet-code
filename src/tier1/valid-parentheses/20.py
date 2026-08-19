453c6ef4575bc6ec8b130c401474ecdc42cb63ee
amir-m-seifi <amirseifi93@gmail.com>
2026-08-19 21:33:40 +0330

    second version

---
+++ src/tier1/valid-parentheses/20.py
@@ -0,0 +1,20 @@
+# 20. Valid Parentheses
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack = []
+        brackets = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in brackets.values():
+                stack.append(char)
+            else:
+                if not stack or stack.pop() != brackets[char]:
+                    return False
+
+        return not stack
