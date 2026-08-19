e3bdb0fb7dba00dcb898ebb3525715d0699ba404
Amir-Seifi <amirseiifi@gmail.com>
2026-08-19 15:46:16 +0330

    JS problems removed

--- src/30day-problems/return-length-of-arguments-passed/2703.js
+++
@@ -1,9 +0,0 @@
-// 2703. Return Length of Arguments Passed
-
-/**
- * @param {...(null|boolean|number|string|Array|Object)} args
- * @return {number}
- */
-var argumentsLength = function (...args) {
-  return args.length;
-};
